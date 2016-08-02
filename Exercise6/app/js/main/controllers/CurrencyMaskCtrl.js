ada81a639e86bc50b3b89e8cae99db7df44ea427
Varun Goel <varun.goel@optimusinfo.com>
2016-08-02 21:38:47 +0530

    Completed the exercise 6

---
+++ Exercise6/app/js/main/controllers/CurrencyMaskCtrl.js
@@ -0,0 +1,8 @@
+app.controller('CurrencyMaskCtrl', ['$scope', function ($scope) {
+    var vm = this;
+    vm.mask = "$ 9";
+
+    vm.updateMask = function () {
+        this.mask = "$ 9,999,999";
+    };
+}]);
---
+++ Exercise6/app/js/main/controllers/PhoneMaskCtrl.js
@@ -0,0 +1,4 @@
+app.controller('PhoneMaskCtrl', ['$scope', function ($scope) {
+    var vm = this;
+
+}]);
